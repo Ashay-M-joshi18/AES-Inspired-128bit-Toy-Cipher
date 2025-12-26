f32fb25e1eda5f4fbe682192bab2a5a66096b6d0
Ashay-M-joshi18 <ashayjoshi81@gmail.com>
2025-12-26 23:25:35 +0530

    Refactor generate_round_key function for clarity

--- key_schedule.py
+++ key_schedule.py
@@ -1,23 +1,32 @@
-from core import shift_col
+from core import form_words, sub_bytes , rotate, Add_Rcon , sbox
 
 # ---------- KEY ----------
 
-def generate_round_key(round_key):
-    all_round_key=[]
-    for i in range(10):
-        if i==0:
-            all_round_key.append(round_key)
-        else:
-            copy_key = [row[:] for row in all_round_key[i-1]]
-            diffused_key=shift_rows(copy_key)
-            
-            diffused_key=shift_col(diffused_key)
-            diffused_key=sub_bytes(diffused_key,inverse =False)
-            temp_round_key=[]
-            for j in range(4):
-                temp_row=[]
-                for k in range(4):
-                    temp_row.append(diffused_key[j][k]^all_round_key[i-1][j][k])
-                temp_round_key.append(temp_row)
-            all_round_key.append(temp_round_key)
-    return all_round_key
+def generate_round_key(initial_key):
+    all_round_keys = []
+    all_round_keys.append(initial_key)
+
+    for r in range(10):
+        prev_key = all_round_keys[r]
+        words = []
+
+        # ---- w0 ----
+        temp = form_words(prev_key, 3)      # w3
+        temp = rotate(temp, 1)              # RotWord
+        for i in range(4):
+            temp[i] = sbox[temp[i]]# SubWord
+        Add_Rcon(temp, r)                   # Rcon
+        w0 = [temp[i] ^ prev_key[i][0] for i in range(4)]
+        words.append(w0)
+
+        # ---- w1, w2, w3 ----
+        for j in range(1, 4):
+            prev_word = form_words(prev_key, j)
+            new_word = [prev_word[i] ^ words[j-1][i] for i in range(4)]
+            words.append(new_word)
+
+        # ---- convert words → round key matrix ----
+        round_key = [[words[c][r_] for c in range(4)] for r_ in range(4)]
+        all_round_keys.append(round_key)
+
+    return all_round_keys
