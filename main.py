64cb70e505dc770fc925b19db3fdf573e5fc7603
Ashay-M-joshi18 <ashayjoshi81@gmail.com>
2025-12-26 23:26:47 +0530

    Refactor input functions for encryption and decryption

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ecb import encrypt_ecb, decrypt_ecb
-from io_utils import get_user_choice, get_message, get_key, print_result
+from io_utils import get_user_choice, get_plaintext, get_master_key, print_encrypted , get_ciphertext , print_decrypted
 
 
 def main():
@@ -7,17 +7,18 @@ def main():
 
     choice = get_user_choice()
 
+
     if choice == 1:
-        message = get_message("Enter your message: ")
-        key = get_key("Enter your master key (16 chars): ")
+        message = get_plaintext()
+        key = get_master_key()
         cipher_blocks = encrypt_ecb(message, key)
-        print_result("Encrypted Message", cipher_blocks)
+        print_encrypted(cipher_blocks)
 
     elif choice == 2:
-        cipher_text = get_message("Enter your ciphertext (hex): ")
-        key = get_key("Enter your master key (16 chars): ")
+        cipher_text = get_ciphertext()
+        key = get_master_key()
         plain_blocks = decrypt_ecb(cipher_text, key)
-        print_result("Decrypted Message", plain_blocks)
+        print_decrypted(plain_blocks)
 
     else:
         print("Invalid choice. Exiting.")
